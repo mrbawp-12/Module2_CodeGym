0db94cb5f4c0142327f9dccb33252fa63de7157b
Phạm Ngọc Đức <pduc8117@gmail.com>
2026-02-06 22:53:54 +0700

    bổ sung code

--- DSA/src/linkedlist_demo/Node.java
+++ DSA/src/linkedlist_demo/Node.java
@@ -19,7 +19,33 @@ class LinkedList<T> {
 
     public void add(T data) {
         Node<T> node = new Node<>(data);
+        if (this.head == null) {
+            this.head = node;
+            this.tail = node;
+        }else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
 
+    public void  showLinkedList() {
+        Node<T> currentNode = this.head;
+        while (true) {
+            System.out.println(currentNode.data + "; ");
+            currentNode = currentNode.next;
+            if (currentNode == null) break;
+        }
+    }
+
+    class Main{
+        public static void main(String[] args) {
+            LinkedList<Integer> list = new LinkedList<>();
+            list.add(1);
+            list.add(12);
+            list.add(45);
+            list.add(33);
+            list.showLinkedList();
+        }
     }
 
 }
